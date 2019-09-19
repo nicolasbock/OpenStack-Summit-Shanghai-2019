fe0f25b2bf7d623399e99fee2d28c97ca72058a4
Nicolas Bock <nicolas.bock@suse.com>
2019-09-19 14:17:17 -0600

    Make the OpenStack create script more robust
    Signed-off-by: Nicolas Bock <nicolas.bock@suse.com>

--- create-vm-OpenStack.sh
+++ create-vm-OpenStack.sh
@@ -9,18 +9,18 @@ export OS_CLOUD=ECP-nbock
 : ${OSH_FLAVOR:=m2.xlarge}
 
 if openstack server show ${OSH_HOSTNAME} > /dev/null; then
-  echo "The server '${OSH_HOSTNAME}' already exists"
-  exit 1
+  echo "the server '${OSH_HOSTNAME}' already exists"
+  openstack server rebuild --image ${OSH_IMAGE} ${OSH_HOSTNAME}
+else
+  openstack server create \
+    --flavor ${OSH_FLAVOR} \
+    --image ${OSH_IMAGE} \
+    --network fixed \
+    --security-group default \
+    --security-group ping/ssh \
+    --key-name nbock \
+    ${OSH_HOSTNAME}
 fi
-
-openstack server create \
-  --flavor ${OSH_FLAVOR} \
-  --image ${OSH_IMAGE} \
-  --network fixed \
-  --security-group default \
-  --security-group ping/ssh \
-  --key-name nbock \
-  ${OSH_HOSTNAME}
 
 while true; do
   OSH_VM_PORT=$(openstack port list \
@@ -32,13 +32,20 @@ while true; do
   fi
 done
 
-openstack floating ip create \
-  --port ${OSH_VM_PORT} \
-  floating
-
 OSH_VM_FIP=$(openstack floating ip list \
-  --column 'Floating IP Address' \
+  --port ${OSH_VM_PORT} \
   --format value \
-  --port ${OSH_VM_PORT})
+  --column "Floating IP Address")
+
+if [[ -z ${OSH_VM_FIP} ]]; then
+  openstack floating ip create \
+    --port ${OSH_VM_PORT} \
+    floating
+
+  OSH_VM_FIP=$(openstack floating ip list \
+    --column 'Floating IP Address' \
+    --format value \
+    --port ${OSH_VM_PORT})
+fi
 
 ssh-keygen -R ${OSH_VM_FIP}
